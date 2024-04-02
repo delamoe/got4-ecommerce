15442aeb6143630efe7d7660310968e9e278c8c6
Eryk de la Montana <eryk+ecommerce@delamoe.com>
2024-04-03 00:23:38 +0600

    Add initial tests and functionality for products app

--- products/tests.py
+++ products/tests.py
@@ -2,7 +2,14 @@
 from django.test import TestCase
 from .models import Product
 
+
 class ProductTestCase(TestCase):
     def test_product_creation(self):
         product = Product.objects.create(name="Test Product", price=10.99)
+        self.assertEqual(product.name, "Test Product")
+
+
+class ProductTests(TestCase):
+    def test_product_creation(self):
+        product = Product.objects.create(name="Test Product", price=100)
         self.assertEqual(product.name, "Test Product")
