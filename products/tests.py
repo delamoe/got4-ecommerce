4bb8174dd0f3ea4a393ce0e3f1ebecd2d2180eda
Eryk de la Montana <eryk+ecommerce@delamoe.com>
2024-04-03 00:19:14 +0600

    adds core apps

---
+++ products/tests.py
@@ -0,0 +1,8 @@
+# products/tests.py
+from django.test import TestCase
+from .models import Product
+
+class ProductTestCase(TestCase):
+    def test_product_creation(self):
+        product = Product.objects.create(name="Test Product", price=10.99)
+        self.assertEqual(product.name, "Test Product")
